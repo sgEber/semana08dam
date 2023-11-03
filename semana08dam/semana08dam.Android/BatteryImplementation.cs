3bbf8bc186032ecc43a5fdcd59d45ea41653a29e
esHakomo <eber.solano@tecsup.edu.pe>
2023-11-03 15:30:42 -0500

    primer cambio

---
+++ semana08dam/semana08dam.Android/BatteryImplementation.cs
@@ -0,0 +1,132 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using semana08dam;
+using semana08dam.Droid;
+using System;
+
+[assembly: Xamarin.Forms.Dependency(typeof(BatteryImplementation))]
+namespace semana08dam.Droid
+{
+    public class BatteryImplementation : IBattery
+    {
+        public BatteryImplementation()
+        {
+        }
+
+        public int RemainingChargePercent
+        {
+            get
+            {
+                try
+                {
+                    using (var filter = new IntentFilter(Intent.ActionBatteryChanged))
+                    {
+                        using (var battery = Application.Context.RegisterReceiver(null, filter))
+                        {
+                            var level = battery.GetIntExtra(BatteryManager.ExtraLevel, -1);
+                            var scale = battery.GetIntExtra(BatteryManager.ExtraScale, -1);
+
+                            return (int)Math.Floor(level * 100D / scale);
+                        }
+                    }
+                }
+                catch
+                {
+                    System.Diagnostics.Debug.WriteLine("Ensure you have android.permission.BATTERY_STATS");
+                    throw;
+                }
+            }
+        }
+
+        public semana08dam.BatteryStatus Status
+        {
+            get
+            {
+                try
+                {
+                    using (var filter = new IntentFilter(Intent.ActionBatteryChanged))
+                    {
+                        using (var battery = Application.Context.RegisterReceiver(null, filter))
+                        {
+                            int status = battery.GetIntExtra(BatteryManager.ExtraStatus, -1);
+                            var isCharging = status == (int)BatteryStatus.Charging || status == (int)BatteryStatus.Full;
+
+                            var chargePlug = battery.GetIntExtra(BatteryManager.ExtraPlugged, -1);
+                            var usbCharge = chargePlug == (int)BatteryPlugged.Usb;
+                            var acCharge = chargePlug == (int)BatteryPlugged.Ac;
+                            bool wirelessCharge = false;
+                            wirelessCharge = chargePlug == (int)BatteryPlugged.Wireless;
+
+                            isCharging = (usbCharge || acCharge || wirelessCharge);
+                            if (isCharging)
+                                return semana08dam.BatteryStatus.Charging;
+
+                            switch (status)
+                            {
+                                case (int)BatteryStatus.Charging:
+                                    return semana08dam.BatteryStatus.Charging;
+                                case (int)BatteryStatus.Discharging:
+                                    return semana08dam.BatteryStatus.Discharging;
+                                case (int)BatteryStatus.Full:
+                                    return semana08dam.BatteryStatus.Full;
+                                case (int)BatteryStatus.NotCharging:
+                                    return semana08dam.BatteryStatus.NotCharging;
+                                default:
+                                    return semana08dam.BatteryStatus.Unknown;
+                            }
+                        }
+                    }
+                }
+                catch
+                {
+                    System.Diagnostics.Debug.WriteLine("Ensure you have android.permission.BATTERY_STATS");
+                    throw;
+                }
+            }
+        }
+
+        public PowerSource PowerSource
+        {
+            get
+            {
+                try
+                {
+                    using (var filter = new IntentFilter(Intent.ActionBatteryChanged))
+                    {
+                        using (var battery = Application.Context.RegisterReceiver(null, filter))
+                        {
+                            int status = battery.GetIntExtra(BatteryManager.ExtraStatus, -1);
+                            var isCharging = status == (int)BatteryStatus.Charging || status == (int)BatteryStatus.Full;
+
+                            var chargePlug = battery.GetIntExtra(BatteryManager.ExtraPlugged, -1);
+                            var usbCharge = chargePlug == (int)BatteryPlugged.Usb;
+                            var acCharge = chargePlug == (int)BatteryPlugged.Ac;
+
+                            bool wirelessCharge = false;
+                            wirelessCharge = chargePlug == (int)BatteryPlugged.Wireless;
+
+                            isCharging = (usbCharge || acCharge || wirelessCharge);
+
+                            if (!isCharging)
+                                return semana08dam.PowerSource.Battery;
+                            else if (usbCharge)
+                                return semana08dam.PowerSource.Usb;
+                            else if (acCharge)
+                                return semana08dam.PowerSource.Ac;
+                            else if (wirelessCharge)
+                                return semana08dam.PowerSource.Wireless;
+                            else
+                                return semana08dam.PowerSource.Other;
+                        }
+                    }
+                }
+                catch
+                {
+                    System.Diagnostics.Debug.WriteLine("Ensure you have android.permission.BATTERY_STATS");
+                    throw;
+                }
+            }
+        }
+    }
+}
